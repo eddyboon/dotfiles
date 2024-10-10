7c0a2b9926eecf9258302b979ac9b883aa465e89
eddyboon <66462284+eddyboon@users.noreply.github.com>
2024-10-11 07:46:21 +1100

    increase update frequency of brew update

--- sketchybar/.config/sketchybar/items/brew.sh
+++ sketchybar/.config/sketchybar/items/brew.sh
@@ -2,7 +2,7 @@
 
 sketchybar --add event brew_update \
            --add item brew right   \
-		   --set brew update_freq=1800 \
+		   --set brew update_freq=120 \
 		   icon=􀐛 \
 		   label=? \
 		   script="$PLUGIN_DIR/brew.sh"\
