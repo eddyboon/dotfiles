e259e14b81f2940e0876b7c6d4d8d31d53b2c95b
eddyboon <66462284+eddyboon@users.noreply.github.com>
2024-10-23 15:12:07 +1100

    Use nvim tree for file navigation instead

--- lazyvim/.config/nvim/lua/plugins/disabled.lua
+++ lazyvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,7 @@
 return {
-	-- this is where the disabled plugins go
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "folke/flash.nvim", enabled = false },
+  -- this is where the disabled plugins go
+  { "nvim-neo-tree/neo-tree.nvim",       enabled = false },
+  { "folke/flash.nvim",                  enabled = false },
+  { "williamboman/mason.nvim",           enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
 }
