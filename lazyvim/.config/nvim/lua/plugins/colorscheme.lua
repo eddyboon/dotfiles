f90ac81d50cc583e1fc7e826ea6e16203227c509
eddyboon <66462284+eddyboon@users.noreply.github.com>
2024-10-24 20:47:55 +1100

    Change colorscheme to catppuccin frappe

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,22 @@
 return {
-	"olimorris/onedarkpro.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme onedark")
-	end,
+  -- OneDark Pro colorscheme
+  -- {
+  --   "olimorris/onedarkpro.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd("colorscheme onedark")
+  --   end,
+  -- }
+  --
+  -- Catppuccin colorscheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin-frappe"
+    end,
+  }
 }
