bc754453a65f973586256c0975a4b930698650be
eddyboon <66462284+eddyboon@users.noreply.github.com>
2024-10-26 18:19:07 +1100

    Added new themes for nvim and alacritty

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -10,13 +10,46 @@ return {
   -- }
   --
   -- Catppuccin colorscheme
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme "catppuccin-macchiato"
+  --   end,
+  -- }
+  --
+  -- Another onedark colorscheme
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("onedark").setup {
+  --       style = "cool",
+  --     }
+  --     require("onedark").load()
+  --   end,
+  -- }
+  --
+  -- Dracula Colorscheme
+  -- {
+  --   "Mofiqul/dracula.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd("colorscheme dracula")
+  --   end,
+  -- }
+  --
+  -- Tokyonight Colorscheme
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "catppuccin-frappe"
+      vim.cmd("colorscheme tokyonight-storm")
     end,
   }
 }
