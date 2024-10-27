4d81234a314a4e207d68b8e8c0103c2e3c8c82f3
eddyboon <66462284+eddyboon@users.noreply.github.com>
2024-10-27 15:17:04 +1100

    Adjust color settings in tmux, nvim and alacritty

--- lazyvim/.config/nvim/lua/config/settings.lua
+++ lazyvim/.config/nvim/lua/config/settings.lua
@@ -1,24 +1,24 @@
 local options = {
-	number = true,
-	relativenumber = true,
-	tabstop = 4,
-	shiftwidth = 4,
-	smartindent = true,
-	mouse = "a",
-	showmode = false,
-	clipboard = "unnamed",
-	swapfile = false,
-	splitright = true,
-	splitbelow = true,
-	ignorecase = true,
-	cursorline = true,
-	-- termguicolors = true,
-	undofile = true,
-	scrolloff = 8,
+  number = true,
+  relativenumber = true,
+  tabstop = 4,
+  shiftwidth = 4,
+  smartindent = true,
+  mouse = "a",
+  showmode = false,
+  clipboard = "unnamed",
+  swapfile = false,
+  splitright = true,
+  splitbelow = true,
+  ignorecase = true,
+  cursorline = true,
+  termguicolors = true,
+  undofile = true,
+  scrolloff = 8,
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 vim.api.nvim_create_autocmd("TermOpen", { command = "startinsert", pattern = "*" })
