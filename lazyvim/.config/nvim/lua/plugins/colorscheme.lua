2d4073396fc8e02d889c39e4d21c5665c0ee0a50
eddyboon <66462284+eddyboon@users.noreply.github.com>
2024-10-27 15:17:16 +1100

    Change colorscheme again...

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -8,31 +8,31 @@ return {
   --     vim.cmd("colorscheme onedark")
   --   end,
   -- }
-  --
+
   -- Catppuccin colorscheme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin-macchiato"
-    end,
-  }
-  --
-  -- Another onedark colorscheme
   -- {
-  --   "navarasu/onedark.nvim",
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
   --   lazy = false,
   --   priority = 1000,
   --   config = function()
-  --     require("onedark").setup {
-  --       style = "cool",
-  --     }
-  --     require("onedark").load()
+  --     vim.cmd.colorscheme "catppuccin-macchiato"
   --   end,
   -- }
-  --
+
+  -- Another onedark colorscheme
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedark").setup {
+        style = "cool",
+      }
+      require("onedark").load()
+    end,
+  }
+
   -- Dracula Colorscheme
   -- {
   --   "Mofiqul/dracula.nvim",
@@ -42,7 +42,7 @@ return {
   --     vim.cmd("colorscheme dracula")
   --   end,
   -- }
-  --
+
   -- Tokyonight Colorscheme
   -- {
   --   "folke/tokyonight.nvim",
